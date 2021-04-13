534fda65d18d38a6773ac452be7085c22f452fe7
Mark XU <selfcs@foxmail.com>
2021-04-13 22:36:36 +0800

    修改一些注释

--- ch01/1-1.c
+++ ch01/1-1.c
@@ -1,3 +1,5 @@
+/* 例1-1 求阶乘 输入一个正整数n 求n! */
+
 #include<stdio.h>  /* 编译预处理命令 */
 int factorial(int n); /* 函数声明 */
 int main(void); /* 主函数 */
@@ -5,7 +7,9 @@ int main(void); /* 主函数 */
 
 int main(void){
 	int n;  /* 变量定义 */
+	printf("请输入一个正整数：\n");
 	scanf("%d", &n);   /* 输入一个整数 */
+	printf("该整数的阶乘是:\n");
 	printf("%d \n", factorial(n));  /* 调用函数计算阶乘 */
 
 	return 0;
